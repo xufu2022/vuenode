2358db8ec42ce87bf3597e12f66000620d0da1ae
Your Name <you@example.com>
2022-01-05 07:29:17 +0000

    user role

--- node-admin/src/routes.ts
+++ node-admin/src/routes.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { Register, Login, AuthenticatedUser, Logout, UpdateInfo, UpdatePassword } from "./controller/auth.controller";
+import { CreateRole, DeleteRole, GetRole, Roles, UpdateRole } from "./controller/role.controller";
+import { CreateUser, DeleteUser, GetUser, UpdateUser, Users } from "./controller/user.controller";
 import { AuthMiddleware } from "./middleware/auth.middleware";
 
 export const routes=(router: Router)=>{
@@ -9,4 +11,16 @@ export const routes=(router: Router)=>{
     router.post('/api/logout',AuthMiddleware, Logout);
     router.put('/api/users/info', AuthMiddleware, UpdateInfo);
     router.put('/api/users/password', AuthMiddleware, UpdatePassword);
+
+    router.get('/api/users', AuthMiddleware, Users);
+    router.post('/api/users', AuthMiddleware, CreateUser);
+    router.get('/api/users/:id', AuthMiddleware, GetUser);
+    router.put('/api/users/:id', AuthMiddleware, UpdateUser);
+    router.delete('/api/users/:id', AuthMiddleware, DeleteUser);
+
+    router.get('/api/roles', AuthMiddleware,  Roles);
+    router.post('/api/roles', AuthMiddleware,  CreateRole);
+    router.get('/api/roles/:id', AuthMiddleware,  GetRole);
+    router.put('/api/roles/:id', AuthMiddleware,  UpdateRole);
+    router.delete('/api/roles/:id', AuthMiddleware,  DeleteRole);
 }
