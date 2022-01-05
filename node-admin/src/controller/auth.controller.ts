61d44fd8842e5235ffdaa5b71f0a45189a0a3815
Your Name <you@example.com>
2022-01-05 00:21:01 +0000

    encrpt pwd

--- node-admin/src/controller/auth.controller.ts
+++ node-admin/src/controller/auth.controller.ts
@@ -1,18 +1,26 @@
-import { Request,Response } from "express";
+import { Request, Response } from "express";
 import { getManager } from "typeorm";
+import { User } from "../entity/user.entity";
 import { RegistrationValidation } from "../validation/register.validation";
+import bcyptjs from 'bcryptjs';
 
-export const Register=(req: Request,res: Response)=>{
-    const body=req.body;
-    const {error}=RegistrationValidation.validate(body);
-    if(error){
+export const Register =  async (req: Request, res: Response)=> {
+    const body = req.body;
+    const { error } = RegistrationValidation.validate(body);
+    if (error) {
         return res.status(400).send(error.details);
     }
-    if(body.password !== body.password_confirm){
+    if (body.password !== body.password_confirm) {
         return res.status(400).send("password do not match");
     }
 
-    const repository=getManager().getRepository
-    
-res.send(req.body);
+    const repository = getManager().getRepository(User);
+  const {password,...user}=  await repository.save({
+        first_name: body.first_name,
+        last_name: body.last_name,
+        email: body.email,
+        password: await bcyptjs.hash(body.password, 10),
+    })
+
+    res.send(user);
 }
